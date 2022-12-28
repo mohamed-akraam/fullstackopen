26cf91ce478ac4c1da879eabc80ed3f5997aef73
Mohamed <7mohamedakram@gmail.com>
2022-12-28 02:16:13 +0200

    add phonebook step6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 let phonebook = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
 
 app.get('/api/persons', (request, response) => {
-  response.json(phonebook)
-})
+  response.json(phonebook);
+});
 
 app.get('/info', (request, response) => {
   // const info = {
@@ -39,33 +39,41 @@ app.get('/info', (request, response) => {
   // response.set('Content-Type', 'text/html')
   response.write(`<p>Phonebook has info for ${phonebook.length} people<p>`);
   response.write(`<p>${new Date()}</p>`);
-  response.end()
+  response.end();
   // response.end(info.date);
-})
+});
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id; 
-  const phone = phonebook.find(item => item.id === Number(id));
-  phone ? response.json(phone)
-  : response.status(404).send(`<h1>Error 404 Page is not found</h1>`)
-})
+  const id = request.params.id;
+  const phone = phonebook.find((item) => item.id === Number(id));
+  phone
+    ? response.json(phone)
+    : response.status(404).send(`<h1>Error 404 Page is not found</h1>`);
+});
 
 app.delete('/api/persons/:id', (req, res) => {
   const id = req.params.id;
-  phonebook = phonebook.filter(item => item.id !== Number(id));
+  phonebook = phonebook.filter((item) => item.id !== Number(id));
   res.status(204).end('Error 204 no content');
   console.log(phonebook);
-})
+});
 
 app.post('/api/persons', (req, res) => {
-  const newPhonebook = {
-    id: Math.round(Math.random() * 1000),
-
-
+  const phone = {
+    id: Math.round(Math.random() * 10000),
+    name: req.body.name,
+    number: req.body.number,
+  };
+  if (!phone.name || !phone.number) {
+    res.status(400).json({ error: 'content is missing' });
+  } else if (phonebook.some((item) => item.name === phone.name)) {
+    res.status(409).json({ error: 'name must be unique' });
+  } else {
+    res.json(phonebook.concat(phone));
   }
-})
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`listening in port ${PORT}`);
-})
+});
