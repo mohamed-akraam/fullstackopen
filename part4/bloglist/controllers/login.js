154f27b5fce7438b33725641b6b58c2101beddfa
Mohamed <7mohamedakram@gmail.com>
2023-05-03 05:02:29 +0300

    fix login method

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const loginRouter = require('express').Router();
 const User = require('../models/user');
 
-loginRouter.get('/', async (request, response) => {
+loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body;
 
   const user = await User.findOne({ username });
